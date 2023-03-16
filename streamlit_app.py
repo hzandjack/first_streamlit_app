fce80eaa68dc873ab940f7cb761c2f4fec7e7477
hzandjack <106161203+hzandjack@users.noreply.github.com>
2023-03-16 16:34:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit
+
+streamlit. title('My Parents New Healthy Diner')
+
+streamlit. header('Breakfast Menu')
+streamlit. title('My Parents New Healthy Diner')
+streamlit. title('My Parents New Healthy Diner')
+streamlit. title('My Parents New Healthy Diner')
+streamlit. title('My Parents New Healthy Diner')
+streamlit. title('My Parents New Healthy Diner')
 streamlit. title('My Parents New Healthy Diner')
